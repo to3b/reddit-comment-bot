180f667d9bb620237bd114df5102e8e81ccbb0ac
Toby <120763300+to3b@users.noreply.github.com>
2024-12-15 13:45:44 +1030

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,8 @@ if not (REDDIT_USERNAME and REDDIT_PASSWORD and REDDIT_CLIENT_ID and REDDIT_CLIE
 # Bot configuration
 TARGET_SUBREDDIT = "CucumberBotTestSub"  # Replace with your desired subreddit
 TARGET_STRING = "test"  # Replace with your desired search string
-REPLY_MESSAGE = 'Okay, I just can’t do this anymore. Do you want me to say “holy hell” like every other free thinking NPC on this godforsaken website?
+REPLY_MESSAGE = """
+Okay, I just can’t do this anymore. Do you want me to say “holy hell” like every other free thinking NPC on this godforsaken website?
 
 Think again, liberal sheep.
 
@@ -23,7 +24,8 @@ I am a true free thinker, like Ben Shapiro and Joe Rogan have taught me to me. I
 
 Call the exorcist! Bishop goes on vacation, fuck off, i was there when they first commented “Google En Passant” but now look at what you’ve turned this meme into. Just like ever other meme posted in r/anarchychess, it’s not funny, never was, never will be. What will he funny is when you all realise you’re just another sheep in the herd.
 
-You want a new response to drop? Then here it is, libtard'  # Replace with your desired reply message
+You want a new response to drop? Then here it is, libtard
+"""  # Replace with your desired reply message
 
 # Bot behavior configuration
 SLEEP_DURATION = 10  # Adjust the sleep duration as needed in seconds
